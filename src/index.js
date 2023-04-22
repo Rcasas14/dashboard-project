fda8d01d4349735c1b27b0a71bf877cac0342a25
Rcasas14 <reymartcasas123@gmail.com>
2023-04-22 08:02:44 +0800

    completed login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import { Carousel, initTE } from "tw-elements";
-initTE({ Carousel });
 
-// Initialization for ES Users
-import {
-    Dropdown,
-    Ripple,
-    initTE,
-  } from "tw-elements";
-  
-  initTE({ Dropdown, Ripple });
+import 'flowbite';
+
+
+import { Modal } from 'flowbite'
+
+const $modalElement = document.querySelector('#modalEl');
+
+const modalOptions = {
+    placement: 'bottom-right',
+    backdrop: 'dynamic',
+    backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+    onHide: () => {
+        console.log('modal is hidden');
+    },
+    onShow: () => {
+        console.log('modal is shown');
+    },
+    onToggle: () => {
+        console.log('modal has been toggled');
+    }
+}
+
+const modal = new Modal($modalElement, modalOptions);
+
+modal.show();
