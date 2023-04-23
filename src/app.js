ce5dfac2021fd649b4e067cb855960a086dbbb01
reycassy14 <reycassy122@gmail.com>
2023-04-23 19:53:41 +0800

    complete first phase of todolist app

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const ItemBtn = document.querySelector(".item-btn");
+const itemValue = document.getElementById("item-textarea");
+const itemUl = document.querySelector('.item-list');
+
+ItemBtn.addEventListener("click", () => {
+  if (itemValue.value == "") {
+    console.log("success");
+  } else {
+    let itemList = document.createElement("li")
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("flex", "items-center", "mb-4");
+
+    let itemInput = document.createElement("input");
+    itemInput.setAttribute("type", "checkbox");
+    itemInput.setAttribute("id", "default-checkbox");
+    itemInput.setAttribute("value", `${itemValue.value}`);
+    itemInput.classList.add(
+      "w-4",
+      "h-4",
+      "text-blue-600",
+      "bg-gray-100",
+      "border-gray-300",
+      "rounded",
+      "focus:ring-blue-500",
+      "dark:focus:ring-blue-600",
+      "dark:ring-offset-gray-800",
+      "focus:ring-2",
+      "dark:bg-gray-700",
+      "dark:border-gray-600"
+    );
+
+    let itemLabel = document.createElement("label");
+    itemLabel.setAttribute("for", "default=checkbox");
+    itemLabel.classList.add(
+      "ml-2",
+      "text-sm",
+      "font-medium",
+      "text-black-900",
+      "dark:text-black-300"
+    );
+    itemLabel.innerText = `${itemValue.value}`;
+
+    itemDiv.appendChild(itemInput);
+    itemDiv.appendChild(itemLabel);
+    itemList.appendChild(itemDiv)
+
+    console.log(itemUl.hasChildNodes())
+    itemUl.appendChild(itemList);
+  }
+
+  console.log("failed");
+});
