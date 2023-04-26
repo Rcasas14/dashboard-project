700fc5a0b5e94b914cd7b1e8bac3ef234f23f933
reycassy14 <reycassy122@gmail.com>
2023-04-26 21:35:20 +0800

    remove cdn link and unnecessary comment codes for PR Review

--- src/index.js
+++ src/index.js
@@ -24,36 +24,3 @@ const modalOptions = {
 const modal = new Modal($modalElement, modalOptions);
 
 modal.show();
-
-// const ItemBtn = document.querySelector('.item-btn')
-// const itemValue = document.getElementById('item-textarea')
-
-// ItemBtn.addEventListener('click', ()=>{
-//   console.log('success')
-//   if (itemValue.value == '') {
-//     setTimeout(() => {
-//       itemValue.setAttribute('placeholder', 'Write a text')
-//   }, 2000);
-
-//   } else {
-//     let itemDiv = document.createElement('div');
-//   itemDiv.classList.add('flex, items-center, mb-4')
-
-//   let itemInput = document.createElement('input');
-//   itemInput.setAttribute('type', 'checkbox')
-//   itemInput.setAttribute('id', 'default-checkbox')
-//   itemInput.setAttribute('value', '')
-//   itemInput.classList.add('w-4', 'h-4', 'text-blue-600', 'bg-gray-100', 'border-gray-300', 'rounded', 'focus:ring-blue-500', 'dark:focus:ring-blue-600', 'dark:ring-offset-gray-800', 'focus:ring-2', 'dark:bg-gray-700', 'dark:border-gray-600');
-  
-//   let itemLabel = document.createElement('label');
-//   itemLabel.setAttribute('for', 'default=checkbox')
-//   itemLabel.classList.add('ml-2', 'text-sm', 'font-medium', 'text-black-900', 'dark:text-black-300')
-//   itemLabel.innerText = 'Sample Item';
-
-//   itemDiv.appendChild(itemInput);
-//   itemDiv.appendChild(itemLabel);
-//   }
-
-
-  
-// })
