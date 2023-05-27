ae07bc15d0395ea812f01364f362f4e39d9c78e7
Rcasas14 <reymartcasas123@gmail.com>
2023-05-27 09:18:26 +0800

    page navigation using express route

--- dash-config.js
+++ dash-config.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const path = require('path')
 const chalk = require('chalk')
 const debug = require('debug')('dash-config')
+//const data = require('./src/data..') the path and name of your .json file
 
 const app = express();
 const PORT = process.env.PORT
+const indexRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -13,10 +15,18 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+indexRouter.route('/').get((req, res) => {
+    res.render('index')
+})
+indexRouter.route('/todolist').get((req, res) => {
+    res.render('todolist')
+    
+})
 
+app.use('/index', indexRouter);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('login');
 })
 
 app.listen(PORT, () => {
